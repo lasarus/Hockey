0aec2ce70b22af03df8a74cf9bdb06f6a231d290
Wertoyou <bjorn.thoren97@gmail.com>
2016-02-07 12:23:56 +0100

    MOAR improvement

--- team3/AttackerLeft.java
+++ team3/AttackerLeft.java
@@ -17,26 +17,29 @@ public class AttackerLeft extends Player {
     public int getNumber() { return Pucko.numbers[index]; }
     public String getName() { return name; }
     public boolean isLeftHanded() { return false; }
+    int a=0;
     public void step() {
         setAimOnStick(true);
         if (hasPuck()) { // If this player has the puck
-            if(getX()<1000) {
-                skate(2000, -2000, MAX_SPEED); // Go for the edge
-            }
-            else if(getX()<0){
-                skate(2630, -100, MAX_SPEED);
-            }
-            else
+            ++a;
+            turn(2600, 91, MAX_TURN_SPEED);
+
+            if(a>2){
                 shoot(2600, -91, MAX_SHOT_SPEED);
+            }   
         }
         else { // If doesn't have puck
-            if(getX()>-500 && getY()>-100){ // and is in zone of play
+            if(getX()>-5000 && getY()>-1000){ // and is in zone of play
                 skate(getPuck(), MAX_SPEED); // Go for the puck
             }
 
-            else if (true) {
-                skate(getPuck().getX()+100,getPuck().getY()+100,(MAX_SPEED/2)); //Stay in good pos
+            else if (getPuck().getX()>860) {
+                skate(getPuck().getX()+1000,getPuck().getY()-450,100); //Stay in good pos
             }
-        }
+
+            else if(true) {
+                skate(getPuck().getX()-10,getPuck().getY()-450,100); //Stay in good pos
+            }
+         }
     }
 }
