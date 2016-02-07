96f4c72c80b424fd4ec53ec0c81f2cd4e9a5d7c8
Lucas Unnerfelt <llasarus@gmail.com>
2016-02-07 11:55:30 +0100

    defender

--- team3/Defender.java
+++ team3/Defender.java
@@ -4,11 +4,14 @@ import java.awt.Color;
 import hockey.api.GoalKeeper;
 import hockey.api.Player;
 import hockey.api.ITeam;
+import hockey.api.Util;
+import java.lang.Math;
 
 public class Defender extends Player {
 
     private int index;
     private String name;
+    private boolean attack = false;
 
     public Defender(int index, String name) {
         this.index = index;
@@ -17,10 +20,50 @@ public class Defender extends Player {
     public int getNumber() { return Pucko.numbers[index]; }
     public String getName() { return name; }
     public boolean isLeftHanded() { return false; }
+
     public void step() {
-        if (hasPuck()) // If we have the puck
-            skate(2600, 0, 1000); // Go for the goal
-        else // Else
-            skate(getPuck(), 1000); // Go for the puck
+
+	int targetX = -500;
+	int targetY = 700;
+
+	if(attack) {
+	    if(getPuck().getSpeed() > 600) {
+		attack = false;
+	    }
+	    else {
+		skate(getPuck(), 1000);
+	    }
+	}
+	else if(getPuck().getSpeed() < 50 &&
+		getPuck().isHeld() &&
+		Util.dist(getX() - getPuck().getX(), getY() - getPuck().getY()) > 1000) {
+		    attack = true;
+	}
+
+	if(hasPuck()) {
+	    if(getX() * getX() < 100) {
+		shoot(2600, 0, 1000);
+	    }
+	    else {
+		skate(0, 0, 1000);
+	    }
+	}
+	else {
+	    if(getPuck().getX() < 0) {
+		skate(getPuck(), 1000);
+	    }
+	    else {
+		int dist = (int)Util.dist(getX() - targetX, getY() - targetY);
+		if(dist > 300)
+		    {
+		    }
+		if(index % 2 == 0) {
+		    skate(-500, 700, dist);
+		}
+		else {
+		    skate(-500, -700, dist);
+		}
+	    }
+	}
     }
 }
